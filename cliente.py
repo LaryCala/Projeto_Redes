a7bb36379d6e98c56c6a45a343c158c14a25073a
LaryCala <145489536+LaryCala@users.noreply.github.com>
2026-06-12 22:52:18 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -4,7 +4,7 @@ import datetime
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 12000))
 nome = input("Digite seu nome: ") #Recebe o nome do usuário, assim pode diferenciar cada um
-print("Digite /sair para encerrar.")
+print("Digite '/sair' para encerrar.")
 client.send(("ENTROU: " + nome).encode("utf-8")) #Envia uma mensagem a todos dentro do servidor sobre quem entrou, o servidor precisa tratar essas mensagens diferente
 
 while True:
